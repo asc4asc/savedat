28c9b95ea058440c73e244bd32a51db1698886db
Andreas <asc4asc@users.noreply.github.com>
2020-12-29 14:00:36 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,14 +1,35 @@
 "use strict";  
 
+putdat(wert) {
+  // Check browser support
+  if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", wert);
+  } else {
+    // document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
+}
+ 
+getdat() {
+ if (typeof(Storage) !== "undefined") {
+    // Retrieve
+    return(localStorage.getItem("lastname"););
+  } else {
+    // document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
+}
+
 class countButton {
   constructor(button) {    
     this.tmp=document.querySelector(button);
-    this.n=this.tmp.innerHTML;
+    // this.n=this.tmp.innerHTML;
+    this.n=getdat();
   }
   doit() {
     this.n=this.n*1+1;  
     this.tmp.style.backgroundColor = "yellow";
     this.tmp.innerText = this.n;
+    savedat(this.n);
   }
 };     
 
